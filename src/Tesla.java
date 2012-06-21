dd2d03914eda6d34f6c77636c05b29025c982ed2
John Zavidniak <johnza459@gmail.com>
2012-06-21 02:11:01 -0700

    Removed hardcoding

--- src/Tesla.java
+++ src/Tesla.java
@@ -1,37 +1,59 @@
-import java.io.IOException;
-
 import irc.IrcClient;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 
 
 public class Tesla {
 	
-	private static final String NICK = "Tesla";
-	
-	private static final String USER = "Tesla";
+	private static final String CONFIG = "config";
 	
 	public static void main(String[] args) {
-		int port = -1;
+		List<String> data = new ArrayList<String>();
+		File file = new File(CONFIG);
+		BufferedReader in;
 		try {
-			port = Integer.parseInt(args[1]);
-		} catch (NumberFormatException e) {
-			System.out.println("port must be a number");
+			in = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Config file does not exist");
 			return;
 		}
-		if (args.length < 2) return;
+		String str;
+		try {
+			while ((str = in.readLine()) != null) {
+				data.add(str);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Unable to process config file");
+			return;
+		}
+		int port;
+		try {
+			port = Integer.parseInt(data.get(1));
+		} catch (NumberFormatException e) {
+			System.out.println("Port must be an integer");
+			return;
+		}
 		IrcClient tesla;
 		try {
-			tesla = new IrcClient(args[0], port, NICK, USER, null);
+			tesla = new IrcClient(data.get(0), port, data.get(2), data.get(3), null);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("Unable to create the irc client");
 			return;
 		}
-		if (args.length >= 3 && args[2].contains(",")) {
-			String[] channels = args[2].split(",");
-			for (String channel : channels) {
-				tesla.join("#" + channel);
-			}
+		if (data.size() < 5) return;
+		for (int i = 4; i < data.size(); i++) {
+			tesla.join("#" + data.get(i));
 		}
 	}
 	
